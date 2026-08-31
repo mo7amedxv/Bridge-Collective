821c4e626fbc75ce7910551c2755b249b7bdfe27
Mohamed <mohamedsecondacc@gmail.com>
2026-08-31 15:36:49 +0300

    added nav dark overlay when nav is visible

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const navBtn = document.querySelector(".hamburger-button");
 const mainNav = document.querySelector(".header-nav");
+const navOverlay = document.querySelector(".nav-overlay");
 navBtn.addEventListener("click", () => {
-  navBtn.classList.toggle("active");mainNav.classList.toggle("opened");
+  navBtn.classList.toggle("active");
+  mainNav.classList.toggle("opened");
+  navOverlay.classList.toggle("active");
 });
